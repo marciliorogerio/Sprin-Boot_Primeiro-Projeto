99c74988d2fed1b79852795ce2fc32a00fded7a9
Marcillius <marciliordsantos@gmail.com>
2025-04-03 17:04:37 -0300

    teste

--- src/main/java/com/projetozeroum/projetozeroum/controller/NomeController.java
+++ src/main/java/com/projetozeroum/projetozeroum/controller/NomeController.java
@@ -23,37 +23,45 @@ public class NomeController {
 	@GetMapping("/obterIniciais")
 	public String obterIniciais(@RequestParam("nomeCompleto") String nomeCompleto) {
 		return nomeService.obterIniciais(nomeCompleto);
+		// obterIniciais?nomeCompleto=João da Silva
 	}
 
 	@GetMapping("/dividirNome")
 	public String[] dividirNome(@RequestParam("nomeCompleto") String nomeCompleto,
 			@RequestParam("separador") String separador) {
 		return nomeService.dividirNome(nomeCompleto, separador);
+		// dividirNome?nomeCompleto=Maria,José,Ana&separador=,
 	}
 
 	@GetMapping("/removerEspacos")
 	public String removerEspacos(@RequestParam("texto") String texto) {
 		return nomeService.removerEspacos(texto);
+		// removerEspacos?texto= Texto com espaços
 	}
 
 	@GetMapping("/obterParte")
 	public String obterParte(@RequestParam("texto") String texto, @RequestParam("inicio") int inicio,
 			@RequestParam("fim") int fim) {
 		return nomeService.obterParte(texto, inicio, fim);
+		// obterParte?texto=ExemploDeTexto&inicio=0&fim=7
 	}
 
 	@GetMapping("/converterMaiusculas")
 	public String converterMaiusculas(@RequestParam("texto") String texto) {
 		return nomeService.converterMaiusculas(texto);
+		// converterMaiusculas?texto=texto pequeno
 	}
 
 	@GetMapping("/converterMinusculas")
 	public String converterMinusculas(@RequestParam("texto") String texto) {
 		return nomeService.converterMinusculas(texto);
+		// converterMinusculas?texto=TEXTO GRANDE
 	}
 
 	@GetMapping("/verificarContem")
 	public boolean verificarContem(@RequestParam("texto") String texto, @RequestParam("busca") String busca) {
 		return nomeService.verificarContem(texto, busca);
+		// verificarContem?texto=Esta é uma frase&busca=frase
 	}
 }
+//Acho que foi!
